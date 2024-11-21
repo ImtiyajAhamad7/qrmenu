38282712314df77d43d3e2ef7ed6a579a46f0a7c
imtiyaj ahamad <imbanty07@gmail.com>
2024-11-21 17:05:02 +0530

    corrected error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import MenuPage from "./component/MenuPage";
 import ReactQRCode from "react-qr-code";
